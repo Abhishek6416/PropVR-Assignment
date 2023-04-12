10591ddee61287e957ec5c467980df67e1246b1a
Abhishek6416 <abhishekkr6416@gmail.com>
2023-04-12 22:03:34 +0530

    done

--- client/src/Config/Api.js
+++ client/src/Config/Api.js
@@ -1,3 +1,4 @@
-// export const API_LINK = 'http://localhost:8000'
+export const API_LINK = 'http://localhost:9000'
 
-export const API_LINK = 'https://propvr-backend-production.up.railway.app/'
+// export const API_LINK = 'https://propvr-backend-production.up.railway.app/'
+// export const API_LINK ='https://propvr-backend-production-9f2a.up.railway.app/'
